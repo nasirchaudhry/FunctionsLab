4367641fc4272ee775d7ee3acc6aa2652a41b8ff
nasirchaudhry <nasirchaudhary@yahoo.com>
2015-07-10 15:43:11 -0400

    3:42PM updates
    Updates

--- Program.cs
+++ Program.cs
@@ -8,8 +8,139 @@ namespace FunctionsLab
 {
     class Program
     {
+        enum review
+        {
+            approved, rejected, escalated
+        };
+
         static void Main(string[] args)
         {
+            double cost = 0;
+            string item = " ";
+            int decision = 0;
+            //string userInput = "no";
+            Console.WriteLine("Enter description of expense: ");
+            item = Console.ReadLine();
+            Console.WriteLine("Enter cost of item to expense: ");
+            cost = double.Parse(Console.ReadLine());
+
+            decision = firstLevel(cost, item);
+            decision = secondLevel(cost, item);
+            decision = thirdLevel(cost, item);
+            decision = fourthLevel(cost, item);
+
+        }
+        public static int firstLevel(double c, string i) 
+        {
+            int choice = 0;            
+            if (i.Contains("entertainment"))
+            {
+                choice = (int)review.rejected;
+            }
+            else if (c > 250) 
+            {
+                Console.WriteLine("Enter 1 to reject or 2 to escalate");
+                choice = int.Parse(Console.ReadLine());
+            }
+            else 
+            {
+                Console.WriteLine("Enter 1 to reject or 2 to escalate and 3 to approve");
+                choice = int.Parse(Console.ReadLine());
+            }
+            if (choice == review.escalated)
+            {
+                secondLevel(c, i);
+            }
+            else
+            {
+                decisionMade(choice);
+            }
+            return choice;
+
+        }
+        public static int secondLevel(double c, string i)
+        {
+            int choice = 0;
+            if (i.Contains("towncars"))
+            {
+                choice = (int)review.rejected;
+            }
+            else if (c > 500)
+            {
+                Console.WriteLine("Enter 1 to reject or 2 to escalate");
+                choice = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Enter 1 to reject or 2 to escalate and 3 to approve");
+                choice = int.Parse(Console.ReadLine());
+            }
+            if (choice == review.escalated)
+            {
+                thirdLevel(c, i);
+            }
+            else
+            {
+                decisionMade(choice);
+            }
+            return choice;
+        }
+        public static int thirdLevel(double c, string i)
+        {
+            int choice = 0;
+            if (i.Contains("hardware"))
+            {
+                choice = (int)review.rejected;
+            }
+            if (c > 1000)
+            {
+                Console.WriteLine("Enter 1 to reject or 2 to escalate");
+                choice = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Enter 1 to reject or 2 to escalate and 3 to approve");
+                choice = int.Parse(Console.ReadLine());
+            }
+            if (choice = review.escalated)
+            {
+                fourthLevel(c, i);
+            }
+            else
+            {
+                decisionMade(choice);
+            }
+            return choice;
+        }
+        public static int fourthLevel(double c, string i)
+        {
+            int choice = 0;
+            
+            Console.WriteLine("Enter 1 to reject or 3 to approve");
+            choice = int.Parse(Console.ReadLine());
+            
+            return choice;
+        }
+        public static string decisionMade(int choice) 
+        {
+            switch(choice)
+            {
+                case 0:
+                    {
+                        Console.WriteLine("Expense is approved");
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("Expense is rejected");
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+                
         }
     }
 }
